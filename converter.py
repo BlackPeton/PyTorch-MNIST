9aff2d806ba9a5bfde678b00815f4755b48b1da5
BlackPeton <atomintogalaxy@gmail.com>
2020-12-17 10:49:36 +0800

    add images converter

--- converter.py
+++ converter.py
@@ -1,8 +1,9 @@
 from utils import convert
 
-DATA_PATH = './DataSets'
-IMAGES_PATH = './Images'
+TRAIN_SET = './DataSets/MNIST/processed/training.pt'
+TEST_SET = './DataSets/MNIST/processed/test.pt'
+SAVE_PATH = './Images/train'
 NUM_TRAIN = 5
 NUM_TEST = 5
 
-convert.toImages(DATA_PATH, IMAGES_PATH, NUM_TRAIN, NUM_TEST)
+convert.toImages(TRAIN_SET, SAVE_PATH, NUM_TRAIN)
--- utils/convert.py
+++ utils/convert.py
@@ -1,18 +1,12 @@
 import matplotlib.pyplot as pylt
 import torch
+import os
 
-from . import dataLoader
 
-def toImages(dataset_path, images_path, num_train, num_test):
-    train_set = dataLoader.loadTrain_set(dataset_path)
-    test_set = dataLoader.loadTest_set(dataset_path)
-    train_data = torch.utils.data.DataLoader(
-        dataset=train_set, batch_size=60000, shuffle=False)
-    test_data = torch.utils.data.DataLoader(
-        dataset=test_set, batch_size=10000, shuffle=False)
-    images_train, labels_train = train_data
-    images_test, labels_test = test_data
-    for i in range(num_train):
-        pylt.imsave(images_path + '/train/' + i + '.png', images_train[i], cmap='Gray')
-    for j in range(num_test):
-        pylt.imsave(images_path + '/test/' + j + '.png', images_test[j], cmap='Gray')
+def toImages(file, save_path, num):
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    data = torch.load(file)
+    for i in range(num):
+        img_name = save_path + '/' + str(i + 1) + '.png'
+        pylt.imsave(img_name, data[0][i], cmap='Greys')
